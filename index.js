b30a7ef492cc38c73836d8e3fb0e6a7f33246406
BonAbc <158251386+BonAbc@users.noreply.github.com>
2025-11-08 20:14:37 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -43,9 +43,12 @@ app.set("trust proxy", 1);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 //app.use(express.static(path.join(__dirname, "public")));
-app.use("/uploads", express.static("/uploads"));
+//app.use("/uploads", express.static("/uploads"));
 //Above for mp4
 app.set("view engine", "ejs");
+// Serve videos from Render persistent disk
+app.use("/images", express.static("/uploads"));
+// URL: /images/xem.mp4 → file location: /uploads/xem.mp4
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
