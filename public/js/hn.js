8b355622e18eaaa11d08028ec8ac5b933fd76ae1
BonAbc <158251386+BonAbc@users.noreply.github.com>
2025-11-08 22:51:48 -0600

    Delete public directory

--- public/js/hn.js
+++
@@ -1,7 +0,0 @@
-//
-const video = document.getElementById("sapVideo");
-const playBtn = document.getElementById("playBtn");
-const pauseBtn = document.getElementById("pauseBtn");
-
-playBtn.addEventListener("click", () => video.play());
-pauseBtn.addEventListener("click", () => video.pause());
