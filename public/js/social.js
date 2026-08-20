f3e915c14b3305df7471c555d4c5a1abece856c3
hieun171 <hieun171@gmail.com>
2026-08-20 04:26:24 -0500

    Update the files

--- public/js/social.js
+++ public/js/social.js
@@ -673,3 +673,14 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+//
+document.addEventListener("click", (event) => {
+  const button = event.target.closest(".social-reply-button");
+
+  if (!button) return;
+
+  const commentId = button.dataset.commentId;
+  const parentReplyId = button.dataset.parentReplyId || null;
+
+  socialShowReply(commentId, parentReplyId);
+});
