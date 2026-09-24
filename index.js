813eb16891ef4bf0516af13fd38b2f10fa4b1d11
hieun171 <hieun171@gmail.com>
2026-09-24 16:39:59 -0500

    Update the file

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.use((req, res, next) => {
 
 const globalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 200,
+  max: 300,
   standardHeaders: true,
   legacyHeaders: false,
   message: "Too many requests. Please try again later.",
